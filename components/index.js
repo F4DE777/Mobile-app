7f8f4cd6847264b564057c401546b75aa28a3cfe
Odesigo <chidera@winexviv.com>
2023-01-31 18:15:12 +0100

    push

--- components/index.js
+++ components/index.js
@@ -2,6 +2,10 @@ import FocusStatusBar from "./FocusStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import { CircleButton,RectButon  } from "./Buton";
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+
+
 export{
-    FocusStatusBar,HomeHeader  ,NFTCard, CircleButton,RectButon
+    FocusStatusBar,HomeHeader  ,NFTCard, CircleButton,RectButon,DetailsBid,DetailsDesc
 }
--- screens/Details.js
+++ screens/Details.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import{SafeAreaView,Text} from 'react-native'
+import {
+  SafeAreaView,
+  View,
+  Text,
+  SafeAreaViewBase,
+  Image,
+  FlatList,
+  StatusBar,
+} from "react-native";
+import { COLORS, SIZES, SHADOWS, FONT, assets } from "../constants";
+import {
+  CircleButton,
+  RectButon,
+  SubInfo,
+  FocusStatusBar,
+  DetailsDesc,
+  DetailsBid,
+} from "../components";
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
 
-const Details = () => {
   return (
-    <SafeAreaView>
-    <Text>
-      igtuygiuhyguhgiuhiuh
-    </Text>
-</SafeAreaView>
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <View>
+        <RectButon minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark}/>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Details
+export default Details;
